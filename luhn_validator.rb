b491908e4a40f129f5a0efbd290ebcae5013af9f
Anita <yachi77128230@gmail.com>
2023-03-09 22:27:36 +0800

    update

--- luhn_validator.rb
+++ luhn_validator.rb
@@ -9,16 +9,15 @@ module LuhnValidator
   def validate_checksum
     nums_a = number.to_s.chars.map(&:to_i)
     # TODO: use the integers in nums_a to validate its last check digit
-    last_digit = nums_a.pop
-    multi_nums = nums_a.reverse.each_with_index { |digit, index|
+    last_digit = nums_a[-1]
+    multi_nums = nums_a[0...-1].reverse.map.with_index do |digit, index|
       if index.even?
         digit * 2 > 9 ? (digit * 2) - 9 : digit * 2
       else
         digit
       end
-    }
+    end
     check_digit = (10 - multi_nums.sum % 10) % 10
-
     check_digit == last_digit
   end
 end
